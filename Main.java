40d815e5d4d233de9521e7ad0addaa13e2255ce1
Fawzia Akter Chowdhury <fawziaakter2022@gmail.com>
2025-09-19 12:23:23 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//ELIGIBLE VOTERS COUNTING!!
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print( "Enter number of people (N)  : ");
+        System.out.print( "STEP 1: Enter number of people : ");
     int N = sc. nextInt();
     int [] A = new int[N] ;
-        System.out.println ( "Enter the ages of people :  ");
+        System.out.println ( "STEP 2: Enter the ages of people :  ");
     for (int i =0 ; i < N ; i++)
     {
         A[i] = sc.nextInt();
     }
-        System.out.println ( "Enter minimum age required to vote (X): ");
+        System.out.println ( "STEP 3: Enter minimum age required to vote : ");
     int X = sc.nextInt();
     int count = 0 ;
 
